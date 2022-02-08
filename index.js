9efafed9df4ecffcdceb925f8526fb39068257c1
Shahidbhatt07 <bhatshahid6542@gmail.com>
2022-02-09 01:57:28 +0530

    Starting javaScript

---
+++ index.js
@@ -0,0 +1,24 @@
+//variable declaration
+var a;
+
+//variable initialisation
+a=10;
+console.log(typeof a);
+console.log(a);
+
+a="hello"; //reinitalise of variable a
+console.log(a);
+
+a=true;
+console.log(a);
+console.log(typeof a);
+
+a=null;
+console.log(a);
+console.log(typeof a);
+
+//Bolean
+var t=true;
+var f=false;
+console.log(t);
+console.log(f);
