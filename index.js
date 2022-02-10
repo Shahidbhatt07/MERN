0091da83295db64d6771462f71c632fe039abd67
Shahidbhatt07 <bhatshahid6542@gmail.com>
2022-02-10 13:30:11 +0530

    JS data types done

--- index.js
+++ index.js
@@ -1,33 +1,51 @@
-// Learning JavaScript
+// // Learning JavaScript
 
-//var let constant
-
-
-//variable declaration
-var a;
-
-//variable initialisation
-a=10;
-console.log(a);
-console.log(typeof a);
+// //var let constant
 
 
-a="hello"; //reinitalise of variable a
-console.log(a);
-console.log(typeof a);
+// //variable declaration
+// var a;
 
-a=true;
-console.log(a);
-console.log(typeof a);
+// //variable initialisation
+// a=10;
+// console.log(a);
+// console.log(typeof a);
 
-a=null;
-console.log(a);
-console.log(typeof a);
 
-//Bolean
-var t=true;
-var f=false;
-console.log(t);
-console.log(typeof a);
-console.log(f);
-console.log(typeof a);
+// a="hello"; //reinitalise of variable a
+// console.log(a);
+// console.log(typeof a);
+
+// a=true;
+// console.log(a);
+// console.log(typeof a);
+
+// a=null;
+// console.log(a);
+// console.log(typeof a);
+
+// //Number
+// var num=10
+// console.log(num);
+
+// var boolean=2.4
+// console.log(boolean);
+
+
+// //Bolean
+// var t=true;
+// var f=false;
+// console.log(t);
+// console.log(typeof a);
+// console.log(f);
+// console.log(typeof a);
+
+//Strins
+//    ""(double quotes),       ''(single quotes),       ``(backtics)      used for strings
+
+var str = 'a'
+console.log(str);
+console.log(typeof str);
+
+str='how u doin?'
+console.log(str);
