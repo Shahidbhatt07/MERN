aefdaf8a2613fca31dd554a400340d60ea20e334
Shahid <bhatshahid6543@gmail.com>
2022-03-24 19:41:45 +0530

    espn_scrapper Started

--- Node/fileOrganizer/main.js
+++ Node/fileOrganizer/main.js
@@ -1,17 +1,22 @@
 // entry point of my command line 
 let helpFunc = require("./commands/help");
+let orgFunc = require("./commands/organize");
+let treeFunc = require("./commands/tree");
 // console.log(helpFunc.ghoda());
 let inputArr = process.argv.slice(2);
+// console.log(inputArr);
 let command = inputArr[0];
 let path = inputArr[1];
 switch (command) { //organizee
     case "tree":
         //call tree function
-        console.log("tree function called and executed succesfully on path " + path);
+        treeFunc.tree(path);
+        // console.log("tree function called and executed succesfully on path " + path);
         break;
     case "organize":
         //call organize function
-        console.log("organize function called and executed succesfully on path "+ path);
+        orgFunc.organize(path);
+        // console.log("organize function called and executed succesfully on path "+ path);
         break;
     case "help":
         //call help function
